85b8408e1145f5411e73c05c4de6de210b587d8b
gdickson52 <gdickson52@gmail.com>
2019-06-29 14:49:56 -0400

    freeshelf

--- freeshelf/urls.py
+++ freeshelf/urls.py
@@ -1,4 +1,4 @@
-"""freeshelf URL Configuration
+"""locallibrary URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,36 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls import include, url
+
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import RedirectView
-from core import views as core_views
+
+
 
 urlpatterns = [
-    path('', core_views.index, name='index'),
-    path('', RedirectView.as_view(url='/index/', permanent=True)),
-    path('books/', core_views.BookListView.as_view(), name='books'),
-    path('book/<int:pk>', core_views.BookDetailView.as_view(), name='book-detail'),
-    path('category/<int:pk>', core_views.CategoriesDetailView.as_view(), name='category-detail'),
     path('admin/', admin.site.urls),
-    path('accounts/', include('registration.backends.simple.urls')),
 ]
-
-# if settings.DEBUG:
-#     import debug_toolbar
-#     urlpatterns = [
-#         path('__debug__/', include(debug_toolbar.urls)),
-
-        # For django versions before 2.0:
-        # url(r'^__debug__/', include(debug_toolbar.urls)),
-
-    # ] + urlpatterns
-
-# Use static() to add url mapping to serve static files during development (only)
-
-from django.conf.urls.static import static
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
